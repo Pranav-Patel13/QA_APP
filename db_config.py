01b78933c0e6dfc381ebb8cf74e077d82b56bc6f
Pranav-Patel13 <161235861+Pranav-Patel13@users.noreply.github.com>
2025-06-12 17:41:10 +0530

    Update db_config.py
    final

--- db_config.py
+++ db_config.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 def get_connection():
     return mysql.connector.connect(
-        host=st.secrets["localhost"],
-        user=st.secrets["root"],
-        password=st.secrets[""],
-        database=st.secrets["word_documents"]
+        host=st.secrets["DB_HOST"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"]
     )
