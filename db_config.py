d1748378e55d0a2e3e866d33289049e9adeaee59
Pranav-Patel13 <161235861+Pranav-Patel13@users.noreply.github.com>
2025-06-12 17:34:49 +0530

    Update db_config.py
    data base changes

--- db_config.py
+++ db_config.py
@@ -1,24 +1,10 @@
-# db_config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '',  # Your MySQL password
-#     'database': 'document_qadb'
-# }
-# db_config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '',  # Your MySQL password
-#     'database': 'word_documents'
-# }
-
-# db_config.py
-
 import mysql.connector
+import streamlit as st
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="word_documents"
+        host=st.secrets["localhost"],
+        user=st.secrets["root"],
+        password=st.secrets[""],
+        database=st.secrets["word_documents"]
     )
