3a0e3c3e361048d40416ce2a7f341064b8979582
Pranav-Patel13 <161235861+Pranav-Patel13@users.noreply.github.com>
2025-06-12 17:32:36 +0530

    Update db_config.py
    data base connection query

--- db_config.py
+++ db_config.py
@@ -4,9 +4,21 @@
 #     'password': '',  # Your MySQL password
 #     'database': 'document_qadb'
 # }
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',  # Your MySQL password
-    'database': 'word_documents'
-}
+# db_config = {
+#     'host': 'localhost',
+#     'user': 'root',
+#     'password': '',  # Your MySQL password
+#     'database': 'word_documents'
+# }
+
+# db_config.py
+
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="word_documents"
+    )
