1f5aff41ddb3b74d61163dce9d4706f6a182e4f9
Pranav-Patel13 <161235861+Pranav-Patel13@users.noreply.github.com>
2025-06-13 11:41:34 +0530

    Update db_config.py
    changed ui

--- db_config.py
+++ db_config.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import streamlit as st
 
-conn = mysql.connector.connect(
-    host=st.secrets["DB_HOST"],
-    port=int(st.secrets["DB_PORT"]),
-    user=st.secrets["DB_USER"],
-    password=st.secrets["DB_PASS"],
-    database=st.secrets["DB_NAME"]
-)
+def get_connection():
+    return mysql.connector.connect(
+        host=st.secrets["DB_HOST"],
+        port=int(st.secrets["DB_PORT"]),
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASS"],
+        database=st.secrets["DB_NAME"]
+    )
